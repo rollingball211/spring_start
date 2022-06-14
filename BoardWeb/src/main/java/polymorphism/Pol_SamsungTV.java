494de5f6e5ca6b911c04e8dfce022f0ccda7cffd
rollingball211 <jingoo211@gmail.com>
2022-06-14 22:02:12 +0900

    update

--- BoardWeb/src/main/java/polymorphism/Pol_SamsungTV.java
+++ BoardWeb/src/main/java/polymorphism/Pol_SamsungTV.java
@@ -4,6 +4,7 @@ package polymorphism;
 public class Pol_SamsungTV implements TV {
 
 	// Polymorph를 이용할 클래스들은 interface TV를 구현한다.
+	private SonySpeaker speaker;
 	
 	public Pol_SamsungTV() {
 		System.out.println("=> SamsungTV 객체 생성 ");
@@ -19,11 +20,13 @@ public class Pol_SamsungTV implements TV {
 	}
 	
 	public void volumeUp() {
+		speaker = new SonySpeaker();
 		System.out.println("삼성티비--------소리올림");
 		
 	}
 	
 	public void volumeDown() {
+		speaker = new SonySpeaker();
 		System.out.println("삼성티비-------소리내림");
 	}
 }
---
+++ BoardWeb/src/main/java/polymorphism/SonySpeaker.java
@@ -0,0 +1,16 @@
+package polymorphism;
+
+public class SonySpeaker {
+
+	public SonySpeaker() {
+		System.out.println("SonySpeaker Object created!");
+	}
+	public void volumeUp() {
+		System.out.println("Volume up");
+	}
+		
+	public void volumeDown() {
+		
+		System.out.println("Volume Down");
+	}
+	}
