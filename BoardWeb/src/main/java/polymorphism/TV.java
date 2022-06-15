a5dd5031d2f109f7025ae74030d3bfce8dac2c0a
rollingball211 <jingoo211@gmail.com>
2022-06-15 21:54:47 +0900

    clear?

--- BoardWeb/src/main/java/polymorphism/TV.java
+++ BoardWeb/src/main/java/polymorphism/TV.java
@@ -6,6 +6,7 @@ public interface TV {
 	public void PowerOff();
 	public void volumeUp();
 	public void volumeDown();
+	//error occured?
 }
 
 //1. 다형성을 이용하기 위해 TV들이 공통으로 가져야 할 Method들을 abstract method로 선언.
